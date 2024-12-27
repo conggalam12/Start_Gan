effa4e61471fe70516bc4610cc9271bc2b6c4d63
conggalam12 <cong01697734940@gmail.com>
2024-12-27 08:30:11 +0700

    update

--- Monet/gen.py
+++ Monet/gen.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+from PIL import Image
 class Block(nn.Module):
     def __init__(self, in_channels, out_channels, down=True, act='relu', use_dropout=False, device="cpu"):
         """
@@ -157,11 +158,23 @@ class Generator(nn.Module):
     
 
 if __name__ == "__main__":
-    sample_input = torch.rand(1,3,256,256)
+    image = Image.open(r"C:\Users\cong_nguyen\Documents\Python\Start_Gan\Monet\img\047da870f6.jpg").convert('RGB')
+    transform_list = []
+    transform_list.append(transforms.ToTensor())
+    transform_list.append(
+            transforms.Normalize(
+                mean=[0.5,0.5,0.5],
+                std=[0.5,0.5,0.5]
+            )
+        )
+    transform = transforms.Compose(transform_list)
+    sample_input = transform(image)
     generator = Generator(3,64,"cpu")
+    generator.load_state_dict(torch.load(r'C:\Users\cong_nguyen\Documents\Python\Start_Gan\Monet\weight\generator_weights.pth',weights_only=True,map_location="cpu"))
     generator.eval()
-    output = generator(sample_input)
+    output = generator(sample_input.unsqueeze(0))
     output = output.squeeze(0)
+    output = output*0.5 + 0.5
     transform = transforms.ToPILImage()
     image = transform(output)
     image.save(r"C:\Users\cong_nguyen\Documents\Python\Start_Gan\Monet\result\demo3.jpg")
